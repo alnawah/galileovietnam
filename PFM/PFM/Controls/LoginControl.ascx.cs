d24d4cb3d43491d5895d76089871adcea9e1e8e5
quangminh1203 <quangminh1203@e56bfdc5-771d-a90f-6564-372ee60e2253>
2010-12-27 10:24:30 +0000

    Add project

---
+++ PFM/PFM/Controls/LoginControl.ascx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PFM.Controls
+{
+    public partial class LoginControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            if (UserName.Text.Trim().Equals("nqminh") && Password.Text.Trim().Equals("nwcnebuchad"))
+            {
+                Session["user"] = "admin";
+                Response.Redirect("~/Pages/Main.aspx");
+            }
+            else
+            {
+                FailureText.Text = "Đăng nhập không đúng. Làm lại đi!";
+            }
+        }
+    }
+}
---
+++ PFM/PFM/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PFM
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] == null)
+            {
+                Response.Redirect("~/Pages/LoginPage.aspx");
+            }
+            else if (Session["user"] == "admin")
+            {
+                Response.Redirect("~/Pages/Main.aspx");
+            }
+        }
+    }
+}
